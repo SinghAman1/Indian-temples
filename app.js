48ac844e23cbe391632958712ee3be5f54c8547a
Aman Singh <amansinghrathhore117@gmail.com>
2021-02-20 23:02:48 +0530

    My first commit

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ const User = require('./models/user')
 
 
 const MongoDBStore = require("connect-mongo")(session);
-// process.env.DB_URL ||
-const dbUrl =  'mongodb://localhost:27017/yelp-camp';
+// 
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl , {
     useNewUrlParser: true,
